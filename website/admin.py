b501bc1adce448754696d74fd1d57e97c7c7bcd0
Marlon Cepeda <warlhunters@gmail.com>
2015-11-25 20:30:27 -0430

    [FIX] reparado el error deonde faltaba la palabra admin y la tabla por transformar en lista

--- website/admin.py
+++ website/admin.py
@@ -32,136 +32,133 @@ class CorraleAdmin(admin.ModelAdmin):
 class InmunocastracioneAdmin(admin.ModelAdmin):
     list_display = ['granja','rastro','periodo_venta','fecha_registro',]
 
-class Inmunocastraciones_extendida(admin.ModelAdmin):
+class Inmunocastraciones_extendidaAdmin(admin.ModelAdmin):
     list_display = ['Inmunocastracion','numero','fecha_aplicacion','fecha_registro',]
 
-class Alimentos_fabrica(admin.ModelAdmin):
+class Alimentos_fabricaAdmin(admin.ModelAdmin):
     list_display = ['nombre','status','fecha_registro',]
 
-class Alimentos_fase(admin.ModelAdmin):
+class Alimentos_faseAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class Alimentos_tipo(admin.ModelAdmin):
+class Alimentos_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre ','descripcion','fecha_registro',]
 
-class Alimento(admin.ModelAdmin):
+class AlimentoAdmin(admin.ModelAdmin):
     list_display = ['fabrica','fase','nombre','presentacion','kg_bulto','tipo_alimento','status','fecha_registro',]
 
-class patologias_grupo(admin.ModelAdmin):
+class patologias_grupoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class patologias(admin.ModelAdmin):
+class patologiasAdmin(admin.ModelAdmin):
     list_display = ['casusa','grupo','causa_muerte','causa_descarte','causa_tratamiento','status','fecha_registro',]
 
-class Medicamentos_laboratorio(admin.ModelAdmin):
+class Medicamentos_laboratorioAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','registro_comercial','fecha_registro',]
 
-class Medicamentos_tipo(admin.ModelAdmin):
+class Medicamentos_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class Medicamento(admin.ModelAdmin):
+class MedicamentoAdmin(admin.ModelAdmin):
     list_display = ['nombre','Laboratorio','registro_ica','presentacion','tipo','status','fecha_registro',]
 
-class Medicamentos_indicacione(admin.ModelAdmin):
+class Medicamentos_indicacioneAdmin(admin.ModelAdmin):
     list_display = ['medicamento','indicacion','descripcion','fecha_registro',]
 
-class Mano_obras_tipo(admin.ModelAdmin):
+class Mano_obras_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class Mano_obra(admin.ModelAdmin):
+class Mano_obraAdmin(admin.ModelAdmin):
     list_display = ['granja','fecha_inicial','fecha_final','tipo','detalle','num_personas_pagadas','valor_total',]
 
-class personal(admin.ModelAdmin):#pendiente con esta tabla me parece q es redundante
+class personalAdmin(admin.ModelAdmin):#pendiente con esta tabla me parece q es redundante
     list_display = ['nombres','apellidos','siglas','centro_costo','fecha_ingreso','fecha_retiro','status','eps','contacto',]
 
-class Insumo(admin.ModelAdmin):
+class InsumoAdmin(admin.ModelAdmin):
     list_display = ['descripcion','Marca','presentacion','unidades_paquetes','status','fecha_registro',]
 
-class Pedidos_tipo(admin.ModelAdmin):
+class Pedidos_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class Pedido(admin.ModelAdmin):
+class PedidoAdmin(admin.ModelAdmin):
     list_display = ['granja','tipo','fecha','subtotal','total','fecha_registro',]
 
-class pedidos_medicamentos_extendido(admin.ModelAdmin):
+class pedidos_medicamentos_extendidoAdmin(admin.ModelAdmin):
     list_display = ['pedido','producto','Cantidad','valor',]
 
-class pedidos_alimentos_extendido(admin.ModelAdmin):
+class pedidos_alimentos_extendidoAdmin(admin.ModelAdmin):
     list_display = ['pedido','referencia','medicado','dosis','fecha_recogida','observaciones','valor',]
 
-class animales_genetica(admin.ModelAdmin):
+class animales_geneticaAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class animale(admin.ModelAdmin):
+class animaleAdmin(admin.ModelAdmin):
     list_display = ['granja','galpon','corrales','lote','edad','num_machos','num_hembras','peso_total','remision','valor_lote','procedencia','genetica','observaciones','status',]
 
-class mortalidad(admin.ModelAdmin):
+class mortalidadAdmin(admin.ModelAdmin):
     list_display = ['granja','galpon','corral','fecha','lote','sexo','causa','cantidad','peso','destino',]
 
-class Compras_tipo(admin.ModelAdmin):
+class Compras_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',]
 
-class Compra(admin.ModelAdmin):
+class CompraAdmin(admin.ModelAdmin):
     list_display = ['granja','tipo','fecha','subtotal','total','fecha_registro',]
 
-class Compras_insumo(admin.ModelAdmin):
-    compra = models.ForeignKey(Compra)
-    producto = models.CharField(max_length=255,blank=False)
-    cantidad = models.CharField(max_length=12, blank=False)
-    valor = models.CharField(max_length=12, blank=False)
+class Compras_insumoAdmin(admin.ModelAdmin):
+    list_display = ['compra','producto','cantidad','valor',]
 
-class Compras_medicamento(admin.ModelAdmin):
+class Compras_medicamentoAdmin(admin.ModelAdmin):
     list_display = ['compra','producto','lote','cantidad','valor',]
 
-class Compras_alimento(admin.ModelAdmin):
+class Compras_alimentoAdmin(admin.ModelAdmin):
     list_display = ['compra','referencia','lote','cantidad','valor','medicado','dosis_medicado','valor_medicado',]
 
-class Medicado(admin.ModelAdmin):
+class MedicadoAdmin(admin.ModelAdmin):
     list_display = ['producto','Principio','referencia','status','fecha_registro',]
 
-class Traslados_animale(admin.ModelAdmin):
+class Traslados_animaleAdmin(admin.ModelAdmin):
     list_display = ['granja','lote_origen','lote_destino','nro_animales','causa',]
 
-class Traslados_alimento(admin.ModelAdmin):
+class Traslados_alimentoAdmin(admin.ModelAdmin):
     list_display = ['granja','origen','destino','referencia','cantidad','valor_flete',]
 
-class ventas(admin.ModelAdmin):
+class ventasAdmin(admin.ModelAdmin):
     list_display = ['lote','num_machos','num_hembras','tipo','peso','cliente','planta_sacrificio','vehiculo','cuarentena','precio_total','remision','costos_flete','pago','fecha_registro',]
 
-class Costos_gasto(admin.ModelAdmin):
+class Costos_gastoAdmin(admin.ModelAdmin):
     list_display = ['granja','Galpon','lote','descripcion','amortizacion','costos','observaciones','fecha_registro',] 
 
-class Consumos_tipo(admin.ModelAdmin):
+class Consumos_tipoAdmin(admin.ModelAdmin):
     list_display = ['nombre','descripcion','fecha_registro',] 
 
-class Consumo(admin.ModelAdmin):
+class ConsumoAdmin(admin.ModelAdmin):
     list_display = ['granja','lote','fecha_registro',] 
 
-class Consumos_farmaco(admin.ModelAdmin):
+class Consumos_farmacoAdmin(admin.ModelAdmin):
     list_display = ['consumo','producto','cantidad','ubicacion',] 
 
-class Consumos_alimento(admin.ModelAdmin):
+class Consumos_alimentoAdmin(admin.ModelAdmin):
     list_display = ['consumo','referencia','cantidad','ubicacion',] 
 
-class Consumos_insumo(admin.ModelAdmin):
+class Consumos_insumoAdmin(admin.ModelAdmin):
     list_display = ['consumo','producto','cantidad','ubicacion',] 
 
-class Salidas_placebo(admin.ModelAdmin):
+class Salidas_placeboAdmin(admin.ModelAdmin):
     list_display = ['lote','num_machos','num_hembras','peso_total','ubicacion','tipo_salida','destino','vehiculo','cuarentena','precio_total','remision','valor_flete','fecha_registro',] 
 
-class Tratamientos(admin.ModelAdmin):
+class TratamientosAdmin(admin.ModelAdmin):
     list_display = ['granja','Galpon','corral','causa','lote','cantidad','edad','medicamento','laboratorio','lote_medicamento','ICA','dosis','duracion','retiro','via_aplicacion','observaciones','responsable','fecha_registro',] 
 
-class Curvas_crecimiento(admin.ModelAdmin):
+class Curvas_crecimientoAdmin(admin.ModelAdmin):
     list_display = ['curva','edad','peso','consumo','fecha_registro',] 
 
-class Metas_pc(admin.ModelAdmin):
+class Metas_pcAdmin(admin.ModelAdmin):
     list_display = ['granja','etapa','peso_inicial','edad_inicial','peso_final','mortalidad','conversion','ganacia_peso','gdp','densidad','dias_permanencia'.'consumo_total','consumo_animal_diario','fecha_registro',] 
 
-class Metas_ceba(admin.ModelAdmin):
+class Metas_cebaAdmin(admin.ModelAdmin):
     list_display = ['granja','peso_inicial','edad_inicial','peso_final','mortalidad','descarte','conversion','ganacia_peso','gdp','densidad','dias_permanencia','consumo_total','consumo_animal_diario','costos_produccion','fecha_registro',] 
 
-class Metas_destete_finalizacione(admin.ModelAdmin):
+class Metas_destete_finalizacioneAdmin(admin.ModelAdmin):
     list_display = ['granja','peso_inicial','edad_inicial','peso_final','mortalidad','descarte','conversion','ganacia_peso','gdp','densidad','dias_permanencia','consumo_total','consumo_animal_diario','costos_produccion','fecha_registro',] 
 
 
