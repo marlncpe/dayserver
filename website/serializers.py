54bf1a4f42c480df943788af825314b94ec571b8
Marlon Cepeda <warlhunters@gmail.com>
2015-12-28 14:05:18 -0430

    [MOD] modificado serializers.website para reemplazar el model.serializer por serializers.HyperlinkedModelSerializer

--- website/serializers.py
+++ website/serializers.py
@@ -39,10 +39,10 @@ class Alimentos_tipoSerializer(serializers.ModelSerializer):
 		model = Alimentos_tipo
 		fields = ('nombre','descripcion','fecha_registro',)
 
-class AlimentoSerializer(serializers.ModelSerializer):
-	fabrica = Alimentos_fabricaSerializer()
-	fase = Alimentos_faseSerializer()
-	tipo_alimento = Alimentos_tipoSerializer()
+class AlimentoSerializer(serializers.HyperlinkedModelSerializer):
+	#fabrica = Alimentos_fabricaSerializer()
+	#fase = Alimentos_faseSerializer()
+	#tipo_alimento = Alimentos_tipoSerializer()
 	class Meta:
 		model = Alimento
 		fields = ('id','fabrica','fase','tipo_alimento','nombre','presentacion','kg_bulto','status','fecha_registro',)
@@ -53,8 +53,8 @@ class Mano_obras_tipoSerializer(serializers.ModelSerializer):
 		model = Mano_obras_tipo
 		fields = ('nombre','descripcion','fecha_registro',)
 
-class Mano_obraSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
+class Mano_obraSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
 	class Meta:
 		model = Mano_obra
 		fields = ('id','granja','fecha_inicial','fecha_final','tipo','detalle','num_personas_pagadas','valor_total',)
@@ -74,21 +74,21 @@ class Pedidos_tipoSerializer(serializers.ModelSerializer):
 		model = Pedidos_tipo
 		fields = ('nombre','descripcion','fecha_registro',)
 
-class PedidoSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	tipo = Pedidos_tipoSerializer()
+class PedidoSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
+	#tipo = Pedidos_tipoSerializer()
 	class Meta:
 		model = Pedido
 		fields = ('id','granja','tipo','fecha','subtotal','total','fecha_registro',)
 
-class pedidos_medicamentos_extendidoSerializer(serializers.ModelSerializer):
-	pedido = PedidoSerializer() 
+class pedidos_medicamentos_extendidoSerializer(serializers.HyperlinkedModelSerializer):
+	#pedido = PedidoSerializer() 
 	class Meta:
 		model = pedidos_medicamentos_extendido
 		fields = ('pedido','producto','Cantidad','valor',)
 
-class pedidos_alimentos_extendidoSerializer(serializers.ModelSerializer):
-	pedido = PedidoSerializer() 
+class pedidos_alimentos_extendidoSerializer(serializers.HyperlinkedModelSerializer):
+	#pedido = PedidoSerializer() 
 	class Meta:
 		model = pedidos_alimentos_extendido
 		fields = ('pedido','referencia','medicado','dosis','fecha_recogida','observaciones','valor',)
@@ -98,27 +98,27 @@ class Compras_tipoSerializer(serializers.ModelSerializer):
 		model = Compras_tipo
 		fields = ('nombre','descripcion','fecha_registro',)
 
-class CompraSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	tipo = Compras_tipoSerializer()
+class CompraSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
+	#tipo = Compras_tipoSerializer()
 	class Meta:
 		model = Compra
 		fields = ('id','granja','tipo','fecha','subtotal','total','fecha_registro',)
 
-class Compras_insumoSerializer(serializers.ModelSerializer):
-	compra = CompraSerializer()
+class Compras_insumoSerializer(serializers.HyperlinkedModelSerializer):
+	#compra = CompraSerializer()
 	class Meta:
 		model = Compras_insumo
 		fields = ('compra','producto','cantidad','valor',)
 
-class Compras_medicamentoSerializer(serializers.ModelSerializer):
-	compra = CompraSerializer()
+class Compras_medicamentoSerializer(serializers.HyperlinkedModelSerializer):
+	#compra = CompraSerializer()
 	class Meta:
 		model = Compras_medicamento
 		fields = ('compra','producto','lote','cantidad','valor',)
 
-class Compras_alimentoSerializer(serializers.ModelSerializer):
-	compra = CompraSerializer()
+class Compras_alimentoSerializer(serializers.HyperlinkedModelSerializer):
+	#compra = CompraSerializer()
 	class Meta:
 		model = Compras_alimento
 		fields = ('compra','referencia','lote','cantidad','valor','medicado','dosis_medicado','valor_medicado',)
@@ -134,9 +134,9 @@ class ventasSerializer(serializers.ModelSerializer):
 		model = ventas
 		fields = ('lote','num_machos','num_hembras','tipo','peso','cliente','planta_sacrificio','vehiculo','cuarentena','precio_total','remision','costos_flete','pago','fecha_registro',)
 
-class Costos_gastoSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	Galpon = GalponombreSerializer()
+class Costos_gastoSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
+	#Galpon = GalponombreSerializer()
 	class Meta:
 		model = Costos_gasto
 		fields = ('granja','Galpon','lote','descripcion','amortizacion','costos','observaciones','fecha_registro',)
@@ -146,27 +146,27 @@ class Consumos_tipoSerializer(serializers.ModelSerializer):
 		model = Consumos_tipo
 		fields = ('nombre','descripcion','fecha_registro',)
 
-class ConsumoSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	tipo = Consumos_tipoSerializer()
+class ConsumoSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
+	#tipo = Consumos_tipoSerializer()
 	class Meta:
 		model = Consumo
 		fields = ('granja','lote','tipo','fecha_registro',)
 
-class Consumos_farmacoSerializer(serializers.ModelSerializer):
-	consumo = ConsumoSerializer()
+class Consumos_farmacoSerializer(serializers.HyperlinkedModelSerializer):
+	#consumo = ConsumoSerializer()
 	class Meta:
 		model = Consumos_farmaco
 		fields = ('consumo','producto','cantidad','ubicacion',) 
 
-class Consumos_alimentoSerializer(serializers.ModelSerializer):
-	consumo = ConsumoSerializer()
+class Consumos_alimentoSerializer(serializers.HyperlinkedModelSerializer):
+	#consumo = ConsumoSerializer()
 	class Meta:
 		model = Consumos_alimento
 		fields = ('consumo','referencia','cantidad','ubicacion',) 
 
-class Consumos_insumoSerializer(serializers.ModelSerializer):
-	consumo = ConsumoSerializer()
+class Consumos_insumoSerializer(serializers.HyperlinkedModelSerializer):
+	#consumo = ConsumoSerializer()
 	class Meta:
 		model = Consumos_insumo
 		fields = ('consumo','producto','cantidad','ubicacion',) 
@@ -176,20 +176,20 @@ class Curvas_crecimientoSerializer(serializers.ModelSerializer):
 		model = Curvas_crecimiento
 		fields = ('curva','edad','peso','consumo','fecha_registro',) 
 
-class Metas_pcSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
+class Metas_pcSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
 	class Meta:
 		model = Metas_pc
 		fields = ('granja','etapa','peso_inicial','edad_inicial','peso_final','mortalidad','conversion','ganacia_peso','gdp','densidad','dias_permanencia','consumo_total','consumo_animal_diario','fecha_registro',) 
 
-class Metas_cebaSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
+class Metas_cebaSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
 	class Meta:
 		model = Metas_ceba
 		fields = ('granja','peso_inicial','edad_inicial','peso_final','mortalidad','descarte','conversion','ganacia_peso','gdp','densidad','dias_permanencia','consumo_total','consumo_animal_diario','costos_produccion','fecha_registro',) 
 
-class Metas_destete_finalizacioneSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
+class Metas_destete_finalizacioneSerializer(serializers.HyperlinkedModelSerializer):
+	#granja = GranjanombreSerializer()
 	class Meta:
 		model = Metas_destete_finalizacione
 		fields = ('granja','peso_inicial','edad_inicial','peso_final','mortalidad','descarte','conversion','ganacia_peso','gdp','densidad','dias_permanencia','consumo_total','consumo_animal_diario','costos_produccion','fecha_registro',) 
@@ -203,89 +203,3 @@ class Recordatorios_extendidoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Recordatorios_extendido
 		fields = ('nombre','descripcion','fecha_registro',) 
-
-'''
-class Patologias_grupoSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Patologias_grupo
-		fields = ('nombre','descripcion','fecha_registro',)
-
-class PatologiasSerializer(serializers.ModelSerializer):
-	grupo = Patologias_grupoSerializer()
-	class Meta:
-		model = Patologias
-		fields = ('id','casusa','grupo','causa_muerte','causa_descarte','causa_tratamiento','status','fecha_registro',)
-
-class animales_geneticaSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = animales_genetica
-		fields = ('nombre','descripcion','fecha_registro',)
-
-class animaleSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	galpon = GalponombreSerializer()
-	corrales = CorraleSerializer()
-	genetica = animales_geneticaSerializer()
-	class Meta:
-		model = animale
-		fields = ('id','granja','galpon','corrales','lote','edad','num_machos','num_hembras','peso_total','remision','valor_lote','procedencia','genetica','observaciones','status',)
-
-class mortalidadSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	galpon = GalponombreSerializer()
-	corral = CorraleSerializer()
-	class Meta:
-		model = mortalidad
-		fields = ('id','granja','galpon','corral','fecha','lote','sexo','causa','cantidad','peso','destino',)
-
-
-class Medicamentos_laboratorioSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Medicamentos_laboratorio
-		fields = ('nombre','descripcion','registro_comercial','fecha_registro',)
-
-class Medicamentos_tipoSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Medicamentos_tipo
-		fields = ('nombre','descripcion','fecha_registro',)
-
-class MedicamentoSerializer(serializers.ModelSerializer):
-	laboratorio = Medicamentos_laboratorioSerializer()
-	tipo = Medicamentos_tipoSerializer()
-	class Meta:
-		model = Medicamento
-		fields = ('id','nombre','Laboratorio','registro_ica','presentacion','tipo','status','fecha_registro',)
-
-class Medicamentos_indicacioneSerializer(serializers.ModelSerializer):
-	medicamento = MedicamentoSerializer()
-	class Meta:
-		model = Medicamentos_indicacione
-		fields = ('medicamento','indicacion','descripcion','fecha_registro',)
-
-class Traslados_animaleSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	class Meta:
-		model = Traslados_animale
-		fields = ('granja','lote_origen','lote_destino','nro_animales','causa',)
-
-class Traslados_alimentoSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	class Meta:
-		model = Traslados_alimento
-		fields = ('granja','origen','destino','referencia','cantidad','valor_flete',)
-
-class Salidas_placeboSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Salidas_placebo
-		fields = ('lote','num_machos','num_hembras','peso_total','ubicacion','tipo_salida','destino','vehiculo','cuarentena','precio_total','remision','valor_flete','fecha_registro',)
-
-class TratamientosSerializer(serializers.ModelSerializer):
-	granja = GranjanombreSerializer()
-	galpon = GalponombreSerializer()
-	corral = CorraleSerializer()
-	class Meta:
-		model = Tratamientos
-		fields = ('granja','Galpon','corral','causa','lote','cantidad','edad','medicamento','laboratorio','lote_medicamento','ICA','dosis','duracion','retiro','via_aplicacion','observaciones','responsable','fecha_registro',) 
-
-
-'''
